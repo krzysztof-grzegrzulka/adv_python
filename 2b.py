e34f7c4b07dc826f8b760766514bb18067a54ac4
Krzysztof Grzegrzulka <19797451+krzysztof-grzegrzulka@users.noreply.github.com>
2021-11-07 13:13:48 +0100

    formatting

--- 2b.py
+++ 2b.py
@@ -6,10 +6,13 @@ def calculations_i(numbers_param):
     return numbers2
 
 # version ii
+
+
 def calculations_ii(numbers_param):
     numbers2 = [number*2 for number in numbers_param]
     return numbers2
-    
+
+
 print(calculations_i([1, 2, 3, 4, 5]))
 
 print(calculations_ii([6, 7, 8, 9, 10]))
