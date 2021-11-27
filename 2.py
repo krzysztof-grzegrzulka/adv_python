ba0534699cb02aeab2e27ed9506b8a6ee6ad3fb6
Krzysztof Grzegrzulka <19797451+krzysztof-grzegrzulka@users.noreply.github.com>
2021-11-27 17:31:43 +0100

    poprawki

--- 2.py
+++ 2.py
@@ -1,73 +1,73 @@
 class Student:
     def __init__(self, first_name, last_name, birth_date, city, street,
                  zip_code, phone):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.birth_date = birth_date
-        self.city = city
-        self.street = street
-        self.zip_code = zip_code
-        self.phone = phone
+        self._first_name = first_name
+        self._last_name = last_name
+        self._birth_date = birth_date
+        self._city = city
+        self._street = street
+        self._zip_code = zip_code
+        self._phone = phone
 
 
 class Library:
     def __init__(self, city, street, zip_code, open_hours: str, phone):
-        self.city = city
-        self.street = street
-        self.zip_code = zip_code
-        self.open_hours = open_hours
-        self.phone = phone
+        self._city = city
+        self._street = street
+        self._zip_code = zip_code
+        self._open_hours = open_hours
+        self._phone = phone
 
     def __str__(self):
-        return (f'Biblioteka w mieście {self.city}, kod: {self.zip_code}, ' +
-                f'o adresie {self.street}. Godziny otwarcie: {self.open_hours}. ' +
-                f'Telefon: {self.phone}')
+        return (f'Biblioteka w mieście {self._city}, kod: {self._zip_code}, ' +
+                f'o adresie {self._street}. Godziny otwarcie: {self._open_hours}. ' +
+                f'Telefon: {self._phone}')
 
 
 class Employee:
     def __init__(self, first_name, last_name, hire_date, birth_date,
                  city, street, zip_code, phone):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.hire_date = hire_date
-        self.birth_date = birth_date
-        self.city = city
-        self.street = street
-        self.zip_code = zip_code
-        self.phone = phone
+        self._first_name = first_name
+        self._last_name = last_name
+        self._hire_date = hire_date
+        self._birth_date = birth_date
+        self._city = city
+        self._street = street
+        self._zip_code = zip_code
+        self._phone = phone
 
     def __str__(self):
-        return (f'Pracownik {self.first_name} {self.last_name} urodzony ' +
-                f'{self.birth_date} został zatrudniony {self.hire_date}. ' +
-                f'Mieszka pod adresem {self.street} w {self.city}, ' +
-                f'{self.zip_code}. Posiada telefon o numerze {self.phone}')
+        return (f'Pracownik {self._first_name} {self._last_name} urodzony ' +
+                f'{self._birth_date} został zatrudniony {self._hire_date}. ' +
+                f'Mieszka pod adresem {self._street} w {self._city}, ' +
+                f'{self._zip_code}. Posiada telefon o numerze {self._phone}')
 
 
 class Order(Student, Employee):
     def __init__(self, employee, student, books, order_date):
         self.employee = employee  # pochodzi z Employee
         self.student = student  # pochodzi ze Student
-        self.books = books
-        self.order_date = order_date
+        self._books = books
+        self._order_date = order_date
 
     def __str__(self) -> str:
         return (f'Zamówenie złożone dla {self.student} przez {self.employee} ' +
-                f'na książki: {self.books}, dnia {self.order_date}')
+                f'na książki: {self._books}, dnia {self._order_date}')
 
 
 class Book(Library):
     def __init__(self, library, publication_date, author_name, author_surname,
                  number_of_pages):
         self.library = library  # pochodzi z Library
-        self.publication_date = publication_date
-        self.author_name = author_name
-        self.author_surname = author_surname
-        self.number_of_pages = number_of_pages
+        self._publication_date = publication_date
+        self._author_name = author_name
+        self._author_surname = author_surname
+        self._number_of_pages = number_of_pages
 
     def __str__(self):
-        return(f'Książka z {self.library} napisana przez {self.author_name} ' +
-               f'{self.author_surname}. Wydano ją {self.publication_date}. ' +
-               f'Ma {self.number_of_pages} stron.')
+        return(f'Książka z {self.library} napisana przez {self._author_name} ' +
+               f'{self._author_surname}. Wydano ją {self._publication_date}. ' +
+               f'Ma {self._number_of_pages} stron.')
 
 
 library1 = Library('Katowice', 'Książkowa 10', '40-555', '07:00-19:00',
--- 3.py
+++ 3.py
@@ -1,36 +1,36 @@
 class Property:
     def __init__(self, area, rooms: int, price, address):
-        self.area = area
-        self.rooms = rooms
-        self.price = price
-        self.address = address
+        self._area = area
+        self._rooms = rooms
+        self._price = price
+        self._address = address
 
     def __str__(self) -> str:
-        return(f'Nieruchomość znajduje się w {self.area}, adres: ' +
-               f'{self.address}. Znajduje się w niej {self.rooms} pokoi. ' +
-               f'Wartość to {self.price}')
+        return(f'Nieruchomość znajduje się w {self._area}, adres: ' +
+               f'{self._address}. Znajduje się w niej {self._rooms} pokoi. ' +
+               f'Wartość to {self._price}')
 
 
 class House(Property):
-    def __init__(self, area, rooms: int, price, address, plot):
-        super().__init__(area, rooms, price, address)
-        self.plot = plot
+    def __init__(self, _area, _rooms: int, _price, _address, _plot):
+        super().__init__(_area, _rooms, _price, _address)
+        self._plot = _plot
 
     def __str__(self) -> str:
-        return(f'Dom znajduje się na {self.area}, adres: ' +
-               f'{self.address}. Znajduje się w nim {self.rooms} pokoi. ' +
-               f'Wartość to {self.price}. Działka ma rozmiar {self.plot} m2.')
+        return(f'Dom znajduje się na {self._area}, adres: ' +
+               f'{self._address}. Znajduje się w nim {self._rooms} pokoi. ' +
+               f'Wartość to {self._price}. Działka ma rozmiar {self._plot} m2.')
 
 
 class Flat(Property):
-    def __init__(self, area, rooms: int, price, address, floor):
-        super().__init__(area, rooms, price, address)
-        self.floor = floor
+    def __init__(self, _area, _rooms: int, _price, _address, _floor):
+        super().__init__(_area, _rooms, _price, _address)
+        self._floor = _floor
 
     def __str__(self) -> str:
-        return(f'Mieszkanie znajduje się w {self.area}, adres: ' +
-               f'{self.address}. Znajduje się w nim {self.rooms} pokoi. ' +
-               f'Wartość to {self.price}. Jest na piętrze {self.floor}.')
+        return(f'Mieszkanie znajduje się w {self._area}, adres: ' +
+               f'{self._address}. Znajduje się w nim {self._rooms} pokoi. ' +
+               f'Wartość to {self._price}. Jest na piętrze {self._floor}.')
 
 
 house = House('osiedle mieszkaniowe', 2, '250000', 'Czerwona 3', '1000')
