14fe38516be69e0c61e489c73856355ee815687e
Krzysztof Grzegrzulka <19797451+krzysztof-grzegrzulka@users.noreply.github.com>
2021-12-18 12:43:33 +0100

    poprawki w klasie Movie

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ api = Api(app)
 
 
 class Movie(Resource):
-    def __init__(self, movieId_c, title_c, genres_c):
-        self._movieId_c = movieId_c
+    def __init__(self, movieid_c, title_c, genres_c):
+        self._movieid_c = movieid_c
         self._title_c = title_c
         self._genres_c = genres_c
 
         @property
-        def movieId_c(self) -> str:
-            return self._movieId_c
+        def movieid_c(self) -> str:
+            return self._movieid_c
 
         @property
         def title_c(self) -> str:
@@ -26,8 +26,10 @@ class Movie(Resource):
         def genres_c(self) -> str:
             return self._genres_c
 
-    def __str__(self):
-        return json.dumps(dict(self), ensure_ascii=False)
+    # def __str__(self):
+    def get(self, movieid_c, title_c, genres_c):
+        # return json.dumps(dict(self), ensure_ascii=False)
+        return json.dumps({'movieid': movieid_c, 'title': title_c, 'genres': genres_c})
 
 
 class HelloWorld(Resource):
@@ -42,18 +44,12 @@ with open('source_files/movies.csv', newline='') as movies_csv:
     reader = csv.reader(movies_csv)
     next(reader, None)
     for movieId, title, genres in reader:
-        movies_list.append(Movie(movieId, title, genres))
+        print(movieId, title, genres)
+        # movies_list.append(Movie(movieId, title, genres))
+        Movie(movieId, title, genres)
 
 api.add_resource(Movie, '/movies')
 api.add_resource(HelloWorld, '/')
-
-# def iter_func(string=movies_str):
-#     return iter(string.splitlines())
-
-
-# movies_str = movies_str.iter_func(movies_str)
-# for line in enumerate(movies_str):
-#     print(line)
 
 if __name__ == '__main__':
     app.run(debug=True)
