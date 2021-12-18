9bf2d433571e177cdfbbdd535b53569417701f1d
Krzysztof Grzegrzulka <19797451+krzysztof-grzegrzulka@users.noreply.github.com>
2021-12-18 19:47:11 +0100

    przeniesienie kodu do funkcji

--- main.py
+++ main.py
@@ -6,16 +6,21 @@ from PIL import Image
 pytesseract.pytesseract.tesseract_cmd = \
     r'C:\Program Files\Tesseract-OCR\tesseract'
 
+
+def ocr_from_image(img_path):
+    img1 = cv2.imread(img1_path)
+    gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+
+    filename = tempfile.NamedTemporaryFile(suffix=".png", delete=False)
+    cv2.imwrite(filename.name, gray)
+
+    text = pytesseract.image_to_string(Image.open(filename.name))
+    print(text)
+
+    cv2.imshow("Image", img1)
+    cv2.imshow("Output", gray)
+    cv2.waitKey(0)
+
+
 img1_path = '20211217_153004422_iOS.jpg'
-img1 = cv2.imread(img1_path)
-gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-
-filename = tempfile.NamedTemporaryFile(suffix=".png", delete=False)
-cv2.imwrite(filename.name, gray)
-
-text = pytesseract.image_to_string(Image.open(filename.name))
-print(text)
-
-cv2.imshow("Image", img1)
-cv2.imshow("Output", gray)
-cv2.waitKey(0)
+ocr_from_image(img1_path)
