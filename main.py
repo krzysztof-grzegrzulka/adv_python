8a17034888a116776bba48779ceaad2992bbf9bb
Krzysztof Grzegrzulka <19797451+krzysztof-grzegrzulka@users.noreply.github.com>
2021-12-19 14:51:17 +0100

    poprawki od pycodestyle

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = \
     r'/opt/homebrew/bin/tesseract'
-    # r'C:\Program Files\Tesseract-OCR\tesseract'
+# r'C:\Program Files\Tesseract-OCR\tesseract'
 
 
 def ocr_from_image(img_path):
@@ -13,15 +13,21 @@ def ocr_from_image(img_path):
     # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     filename = tempfile.NamedTemporaryFile(suffix=".png", delete=False)
-    # gray = cv2.threshold(cv2.GaussianBlur(img, (5, 5), 0), 0, 255, cv2.THRESH_BINARY
-    #                      + cv2.THRESH_OTSU)[1]
-    # gray = cv2.threshold(cv2.bilateralFilter(img, 5, 75, 75), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    # gray = cv2.threshold(cv2.medianBlur(img, 3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    # gray = cv2.adaptiveThreshold(cv2.GaussianBlur(img, (5, 5), 0), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-    #                              cv2.THRESH_BINARY, 31, 2)
-    # gray = cv2.adaptiveThreshold(cv2.bilateralFilter(img, 9, 75, 75), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-    #                              cv2.THRESH_BINARY, 31, 2)
-    gray = cv2.adaptiveThreshold(cv2.medianBlur(img, 3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+    gray = cv2.threshold(cv2.GaussianBlur(img, (5, 5), 0), 0, 255,
+                         cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    gray = cv2.threshold(cv2.bilateralFilter(img, 5, 75, 75), 0, 255,
+                         cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    gray = cv2.threshold(cv2.medianBlur(img, 3), 0, 255,
+                         cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    gray = cv2.adaptiveThreshold(cv2.GaussianBlur(img, (5, 5), 0), 255,
+                                 cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                 cv2.THRESH_BINARY, 31, 2)
+    gray = cv2.adaptiveThreshold(cv2.bilateralFilter(img, 9, 75, 75), 255,
+                                 cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                 cv2.THRESH_BINARY, 31, 2)
+    gray = cv2.adaptiveThreshold(cv2.medianBlur(img, 3), 255,
+                                 cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                 cv2.THRESH_BINARY, 31, 2)
     cv2.imwrite(filename.name, gray)
 
     text = pytesseract.image_to_string(Image.open(filename.name))
