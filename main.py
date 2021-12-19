e626cfe1cbb7217d255d69118393d00203fe625d
Krzysztof Grzegrzulka <19797451+krzysztof-grzegrzulka@users.noreply.github.com>
2021-12-19 14:13:35 +0100

    WIP - OCR z captchy

--- main.py
+++ main.py
@@ -8,19 +8,31 @@ pytesseract.pytesseract.tesseract_cmd = \
 
 
 def ocr_from_image(img_path):
-    img1 = cv2.imread(img1_path)
-    gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img = cv2.imread(img_path, 0)
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     filename = tempfile.NamedTemporaryFile(suffix=".png", delete=False)
+    # gray = cv2.threshold(cv2.GaussianBlur(img, (5, 5), 0), 0, 255, cv2.THRESH_BINARY
+    #                      + cv2.THRESH_OTSU)[1]
+    # gray = cv2.threshold(cv2.bilateralFilter(img, 5, 75, 75), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    # gray = cv2.threshold(cv2.medianBlur(img, 3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    # gray = cv2.adaptiveThreshold(cv2.GaussianBlur(img, (5, 5), 0), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    #                              cv2.THRESH_BINARY, 31, 2)
+    # gray = cv2.adaptiveThreshold(cv2.bilateralFilter(img, 9, 75, 75), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    #                              cv2.THRESH_BINARY, 31, 2)
+    gray = cv2.adaptiveThreshold(cv2.medianBlur(img, 3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
     cv2.imwrite(filename.name, gray)
 
     text = pytesseract.image_to_string(Image.open(filename.name))
     print(text)
 
-    cv2.imshow("Image", img1)
+    cv2.imshow("Image", img)
     cv2.imshow("Output", gray)
     cv2.waitKey(0)
 
 
-img1_path = '20211217_153004422_iOS.jpg'
-ocr_from_image(img1_path)
+# img1_path = '20211217_153004422_iOS.jpg'
+# ocr_from_image(img1_path)
+
+captcha1 = 'captcha1.jpg'
+ocr_from_image(captcha1)
